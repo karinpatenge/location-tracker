e2159e5b69208a845b395dfa5bc1ebfd87fa6128
karinpatenge <37591997+karinpatenge@users.noreply.github.com>
2022-11-09 17:51:58 +0100

    Update 01-grant_permissions

--- 01-grant_permissions.sql
+++ 01-grant_permissions.sql
@@ -1,6 +1,12 @@
--- Local DB or DBCS: Run as SYS or SYSTEM 
--- Autonomous DB: Run as ADMIN
+-- In Local DB or DBCS: Run as SYS or SYSTEM 
+grant aq_administrator_role, create job, manage scheduler to testuser;
 
+grant execute on dbms_aq to testuser;
+grant execute on dbms_aqadm to testuser;
+grant select_catalog_role to testuser;
+
+
+-- In Autonomous DB: Run as ADMIN
 grant aq_administrator_role, create job, manage scheduler to testuser;
 
 grant execute on dbms_aq to testuser;
