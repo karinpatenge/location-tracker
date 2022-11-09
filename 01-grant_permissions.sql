8ccd85ccdd6f1f6a1c91f551f0c0ea7ab354bb22
karinpatenge <37591997+karinpatenge@users.noreply.github.com>
2022-11-09 17:36:39 +0100

    Sample scripts
    Script 01 to grant permissions to the DB user.
    Script 02 to execute a sample location tracking workflow.

---
+++ 01-grant_permissions.sql
@@ -0,0 +1,11 @@
+-- Local DB or DBCS: Run as SYS or SYSTEM 
+-- Autonomous DB: Run as ADMIN
+
+grant aq_administrator_role, create job, manage scheduler to testuser;
+
+grant execute on dbms_aq to testuser;
+grant execute on dbms_aqadm to testuser;
+grant execute on dbms_lock to testuser;
+grant execute on dbms_aqin to testuser;
+grant execute on dbms_aqjms to testuser;
+grant select_catalog_role to testuser;
